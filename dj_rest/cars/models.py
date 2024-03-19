5032fa3951efbf8e911b2850fd7a081536d88462
Henievich <henievich@gmail.com>
2024-03-19 20:12:08 +0300

    commit

--- dj_rest/cars/models.py
+++ dj_rest/cars/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Car(models.Model):
+    model = models.CharField(max_length=60)
+    color = models.CharField(max_length=50)
+    year = models.IntegerField()
+    price = models.DecimalField(max_digits=100, decimal_places=2)
+
+    def __str__(self):
+        return f'{self.model} | {self.color} | {self.year}'
