b32338ab54f30f746ffe78b12882340662fb26c5
Yauheni <117765323+ILUTM@users.noreply.github.com>
2024-03-30 14:59:17 +0300

    Delete dj_rest directory

--- dj_rest/cars/serializers.py
+++
@@ -1,8 +0,0 @@
-from .models import Car
-from rest_framework import serializers
-
-
-class CarSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Car
-        fields = ('model', 'year', 'color', 'price')
